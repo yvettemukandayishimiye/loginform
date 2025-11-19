27c9cee7b0a03672f7020d2ac8eeddfc29860300
kevine <kevine@gmail.com>
2025-11-19 20:02:09 +0200

    gfger

--- loginform.php
+++ loginform.php
@@ -16,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
+    $stmt = $conn->prepare("SELECT name FROM users WHERE username = ?");
     $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
